9971093677d77c38dc3558d766f3f28bf122e1d0
Christopher-OOC <olojedechristopher24@gmail.com>
2024-06-19 10:52:43 +0100

    message

---
+++ src/main/java/com/example/rest_assured_test/MobileAppWsRestAssuredTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.rest_assured_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWsRestAssuredTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsRestAssuredTest1Application.class, args);
+	}
+
+}
